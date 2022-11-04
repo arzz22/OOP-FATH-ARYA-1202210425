c4c32811e2205322bf8d1f12329c8cf2b5e01842
Arya Dayusma M <107087110+arzz22@users.noreply.github.com>
2022-11-04 16:55:26 +0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,17 +1,28 @@
-package MODUL1;
+package MODULTP1;
 
 import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) {
+        String tampilan_menu;
         // TODO Create Database
 
         // TODO Create Menu
 
         // TODO Insert Menu to Database
 
-
+        System.out.println("Selamat Datang di Reestoran EAD"
+        + "\nSilahkan Register Terlebih Dahulu"
+        + "\n==================================");
+        tampilan_menu = "\n=================================="
+                            + "\n1. Menu"
+                            + "\n2. Pilih Menu"
+                            + "\n3. Keluar"
+                        +"\n==================================";
+        }
+}
+        
         // TODO Display Main Menu
 
         // TODO Create User
