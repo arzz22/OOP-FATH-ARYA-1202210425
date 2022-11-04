c79cf85111ead7eee7302cad76c1df8ec327ef4c
Arya Dayusma M <107087110+arzz22@users.noreply.github.com>
2022-11-04 16:54:53 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,8 +1,23 @@
-public class User {
+import java.util.Scanner;
 
+public class User {
+    static void userpraktikan(){
+    String username;
+    double NoHp;
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Nama: ");
+    username = input.next();
+    
+    System.out.println("No. Handphone: ");
+    NoHp = input.nextInt();
+
+    System.out.println("\nRegister Succes"
+                        + "\nName : " + username
+                        + "\nPhone Number: " + NoHp);
     // TODO Create Attribute of User; Name and Phone Number then Create Setter
 
     // TODO Create Method to Register User and Display User's Name and Phone Number and success message
 
-
+    }
 }
