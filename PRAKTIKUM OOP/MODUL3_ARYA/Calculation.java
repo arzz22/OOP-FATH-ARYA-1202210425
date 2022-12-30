b7d3cd2868f1a6d9cdf7ec8c76deb7993047b832
Arya Dayusma M <107087110+arzz22@users.noreply.github.com>
2022-12-30 16:42:44 +0700

    git push

---
+++ PRAKTIKUM OOP/MODUL3_ARYA/Calculation.java
@@ -0,0 +1,70 @@
+package MODUL3_ARYA;
+
+public class Calculation implements Runnable {
+    double radius;
+    double side;
+    double area;
+    double phi = 3.14;
+    
+    @Override
+    public void run() {
+        System.out.println("=====Program will start in=====");
+        for (int i = 3; i <= 3; i--) {
+            try {
+                System.out.println("Starting with thread "+i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Error");
+            }
+                
+        }
+    }
+
+    public void setSquare(double side) {
+        double squareArea = side * side;
+        try {
+            if (side < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        this.area = squareArea;
+    }
+
+    public double getSquare() {
+        return area;
+    }
+
+    public void setCircle(double radius, double phi) {
+        double circleArea = phi * (radius * radius);
+        try {
+            if (side < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        this.area = circleArea;
+    }
+
+    public double getCircle() {
+        return area;
+    }
+
+    public void setTrapezoid(double a, double b, double t) {
+        double trapezoidArea = (a + b) * t;
+        try {
+            if (side < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        this.area = trapezoidArea;
+    }
+
+    public double getTrapezoid() {
+        return area;
+    }
+}   
